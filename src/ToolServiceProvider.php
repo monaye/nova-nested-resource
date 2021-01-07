8fefd79c7420f04b6c121a54c76664e769896ed4
Monaye Win <>
2021-01-06 16:12:24 -0800

    add custom collections to tool service provider

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace Monaye\NovaNestedResourceTools;
 
+use Laravel\Nova\Nova;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Nova\Events\ServingNova;
-use Laravel\Nova\Nova;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Monaye\NovaNestedResourceTools\Http\Middleware\Authorize;
 
 class ToolServiceProvider extends ServiceProvider
@@ -21,6 +23,49 @@ class ToolServiceProvider extends ServiceProvider
 
         $this->app->booted(function () {
             $this->routes();
+
+            Collection::macro('flattenTree', function ($childrenField = 'children', $levelAttribute = 'level')
+            {
+                $toProcess = $this->items;
+                $processed = [];
+                while($item = array_shift($toProcess))
+                {
+                    $item->$levelAttribute ++;
+                    $processed[] = $item;
+                    if (count($item->$childrenField) > 0) {
+                        $children = array_reverse($item->$childrenField->items);
+                        foreach ($children as $child) {
+                            $child->$levelAttribute = $item->$levelAttribute;
+                            array_unshift($toProcess,$child);
+                        }
+                    }
+                }
+                return Collection::make($processed);
+            });
+    
+             /**
+             * Paginate a standard Laravel Collection.
+             *
+             * @param int $perPage
+             * @param int $total
+             * @param int $page
+             * @param string $pageName
+             * @return array
+             */
+            Collection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') {
+                $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+    
+                return new LengthAwarePaginator(
+                    $this->forPage($page, $perPage),
+                    $total ?: $this->count(),
+                    $perPage,
+                    $page,
+                    [
+                        'path' => LengthAwarePaginator::resolveCurrentPath(),
+                        'pageName' => $pageName,
+                    ]
+                );
+            });
         });
 
         Nova::serving(function (ServingNova $event) {
